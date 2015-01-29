074800f23f7c80723c32fbf952ec7990aa2ce323
Soren <soren.erdweg@cern.ch>
2015-01-29 11:12:59 +0100

    (resolution_histos) Included a truth matcher and gerenral pT resolution histos
    For each particle resolution histograms are included after the trigger and ID
    requirement.

--- specialAna.cc
+++ specialAna.cc
@@ -70,10 +70,11 @@ specialAna::specialAna( const Tools::MConfig &cfg ) :
 
     for(unsigned int i=0;i<4;i++){
         //str(boost::format("N_{%s}")%particleLatex[i] )
-        HistClass::CreateHisto("num",particles[i].c_str(), 40, 0, 39,            TString::Format("N_{%s}", particleSymbols[i].c_str()) );
-        HistClass::CreateHisto(3,"pt",particles[i].c_str(), 5000, 0, 5000,       TString::Format("p_{T}^{%s} (GeV)", particleSymbols[i].c_str()) );
-        HistClass::CreateHisto(3,"eta",particles[i].c_str(), 80, -4, 4,          TString::Format("#eta_{%s}", particleSymbols[i].c_str()) );
-        HistClass::CreateHisto(3,"phi",particles[i].c_str(), 40, -3.2, 3.2,      TString::Format("#phi_{%s} (rad)", particleSymbols[i].c_str()) );
+        HistClass::CreateHisto("num",particles[i].c_str(), 40, 0, 39,               TString::Format("N_{%s}", particleSymbols[i].c_str()) );
+        HistClass::CreateHisto(3,"pt",particles[i].c_str(), 5000, 0, 5000,          TString::Format("p_{T}^{%s} (GeV)", particleSymbols[i].c_str()) );
+        HistClass::CreateHisto("pt_resolution",particles[i].c_str(), 1000, -10, 10, TString::Format("(p_{T}^{reco} - p_{T}^{gen})/p_{T}^{gen}(%s)", particleSymbols[i].c_str()) );
+        HistClass::CreateHisto(3,"eta",particles[i].c_str(), 80, -4, 4,             TString::Format("#eta_{%s}", particleSymbols[i].c_str()) );
+        HistClass::CreateHisto(3,"phi",particles[i].c_str(), 40, -3.2, 3.2,         TString::Format("#phi_{%s} (rad)", particleSymbols[i].c_str()) );
 
         if(not runOnData){
             HistClass::CreateHisto(1,"num_Gen",particles[i].c_str(), 40, 0, 39,        TString::Format("N_{%s}", particleSymbols[i].c_str()) );
@@ -1570,6 +1571,45 @@ void specialAna::Fill_Particle_histos(int hist_number, pxl::Particle* lepton){
     HistClass::Fill(hist_number,str(boost::format("%s_pt")%name ),lepton->getPt(),weight);
     HistClass::Fill(hist_number,str(boost::format("%s_eta")%name ),lepton->getEta(),weight);
     HistClass::Fill(hist_number,str(boost::format("%s_phi")%name ),lepton->getPhi(),weight);
+    if(hist_number == 2){
+        pxl::Particle* match = Get_Truth_match(name, lepton);
+        if(match != 0){
+            HistClass::Fill(name + TString::Format("_pt_resolution"),(lepton->getPt() - match->getPt())/match->getPt(),weight);
+        }
+    }
+}
+
+pxl::Particle* specialAna::Get_Truth_match(string name, pxl::Particle* lepton){
+    double part_temp_eta = lepton->getEta();
+    double part_temp_phi = lepton->getPhi();
+    int part_temp_id = 0;
+    if(name == "Tau"){
+        part_temp_id = 15;
+    }else if(name == "MET"){
+        part_temp_id = 12;
+    }else if(name == "Muon"){
+        part_temp_id = 13;
+    }else if(name == "Ele"){
+        part_temp_id = 11;
+    }
+    double temp_delta_r = 10;
+    pxl::Particle* gen_match = 0;
+    for( vector< pxl::Particle* >::const_iterator part_it = S3ListGen->begin(); part_it != S3ListGen->end(); ++part_it ) {
+        pxl::Particle *part_i = *part_it;
+        int part_temp_truth_id = 0;
+        if(b_8TeV) {
+            part_temp_truth_id = TMath::Abs(part_i->getUserRecord("id").asInt32());
+        }else if(b_13TeV) {
+            part_temp_truth_id = TMath::Abs(part_i->getPdgNumber());
+        }
+        if(part_temp_id != part_temp_truth_id) continue;
+        double test_delta_r = sqrt(pow(part_temp_eta - part_i->getEta(),2) + pow(part_temp_phi - part_i->getPhi(),2));
+        if(test_delta_r < temp_delta_r){
+            temp_delta_r = test_delta_r;
+            gen_match = part_i;
+        }
+    }
+    return gen_match;
 }
 
 double specialAna::DeltaPhi(double a, double b) {
