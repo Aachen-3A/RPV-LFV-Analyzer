4f0482d2d52574a502dc23e60b2c60e76a5b44c1
Soren <soren.erdweg@cern.ch>
2015-02-13 14:40:41 +0100

    Fixed the muon pT resolution
    Up to now the muon pT resolution was calculated as any other particle, but
    as muons are only measured via their track 1/pT is the interesting variable.
    Therefore the resolution for muons was changed accordingly.

---
+++ doc/doc_pT_resolution_fix/html/navtreeindex1.js
@@ -0,0 +1,7 @@
+var NAVTREEINDEX1 =
+{
+"specialAna_8cc.html#afd5c58022c53285421d7299683d5d720":[3,0,2,1],
+"specialAna_8cc_source.html":[3,0,2],
+"specialAna_8hh.html":[3,0,3],
+"specialAna_8hh_source.html":[3,0,3]
+};
